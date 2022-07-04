2ef7530f942db3bdeaa1e4748f92f2af8459a0f5
pink <pink@naver.com>
2022-07-04 12:41:43 +0900

    이미지 업로드 구현

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './index.scss';
-
-const MainPage = (props) => {
+import axios from 'axios';
+import useAsync from '../customHook/useAsync';
+import MainProduct from './MainProduct';
+async function getProducts(){
+    const response = await axios.get("http://localhost:3000/products")
+    return response.data;
+}
+const MainPage = () => {
+    const [state, refetch] = useAsync(getProducts,[])
+    const { loading, data, error} = state;
+    if(loading) return <div>로딩중......</div>
+    if(error) return <div>에러가 발생했습니다.</div>
+    if(!data) return <div>로딩중입니다.</div>
     return (
         <div>
-            
             <div id="main">
                 <div id="banner">
                     <img src="images/banners/banner1.png" alt="" />
@@ -12,82 +22,10 @@ const MainPage = (props) => {
                 <div id="product-list" className='inner'>
                     <h2>그린조명 최신상품</h2>
                     <div id="product-items">
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt="" />아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt="" />아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt="" />아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt="" />아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt="" />아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt="" />아무나
-                                </div>
-                            </div>
-                        </div>
+                        {data.map(product=><MainProduct key={product.id} product={product}/>)}
                     </div>
                 </div>
             </div>
-            
         </div>
     );
 };
--- src/product/index.js
+++ src/product/index.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import "./product.scss";
-
-const ProductPage = () => {
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import useAsync from '../customHook/useAsync';
+async function getProduct(id){
+    const response = await axios.get(`http://localhost:3000/product/${id}`);
+    return response.data;
+}
+const ProductPage = () => { 
+    //product/1
+    const { id } = useParams();
+    const [state] = useAsync(()=>getProduct(id),[id]);
+    const { loading, data:product, error } = state;
+    if(loading) return <div>로딩중입니다.......</div>;
+    if(error) return <div>에러가 발생했습니다.</div>;
+    if(!product) return null;
     return (
         <div className='inner'>
             <div id="image-box">
-                <img src="images/products/product1.jpg" alt=""/>
+                <img src={product.imageUrl} alt=""/>
             </div>
             <div id="profile-box">
                 <ul>
                     <li>
                         <div>
-                            <img src="images/icons/avatar.png" alt=""/>
-                            <span>그린</span>
+                            <img src="/images/icons/avatar.png" alt=""/>
+                            <span>{product.seller}</span>
                         </div>
                     </li>
                     <li>
-                        제품명 새로운 조명
+                        {product.name}
                     </li>
                     <li>
-                        가격 50000원
+                        가격 {product.price}원
                     </li>
                     <li>등록일 2022년 6월 2일</li>
                     <li>상세설명 </li>
